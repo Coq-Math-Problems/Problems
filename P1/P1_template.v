416b338fd973bd57cacf3cb0509fb0d933975f6e
Coq-Math-Problems <coqmathproblems@gmail.com>
2017-05-11 14:16:55 -0500

    Update P1_template.v

--- P1/P1_template.v
+++ P1/P1_template.v
@@ -2,5 +2,5 @@ Definition decr(f : nat -> nat) := forall n, f (S n) <= f n.
 
 Definition valley(f : nat -> nat)(n x : nat) := forall y, x <= y -> y <= n+x -> f y = f x.
 
-Lemma decr_valleys : forall n f, decr f -> exists x, valley f n x.
+Theorem decr_valleys : forall n f, decr f -> exists x, valley f n x.
 Admitted.
